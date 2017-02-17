c28cf28b784b9f77523d42b52ee8b4959a2735ea
Jay <jaybee.codes@gmail.com>
2017-02-17 19:28:25 +0000

    started using mongodb

--- app.js
+++ app.js
@@ -1,33 +1,51 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-var campgrounds =[
-    {name:"Salmon Creek", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-    {name:"Sparrow Hills", image:"https://farm8.staticflickr.com/7268/7121859753_e7f787dc42.jpg"},
-    {name:"Rock Mountain", image:"https://farm9.staticflickr.com/8161/7360193870_cc7945dfea.jpg"},
-    {name:"Salmon Creek", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-    {name:"Sparrow Hills", image:"https://farm8.staticflickr.com/7268/7121859753_e7f787dc42.jpg"},
-    {name:"Rock Mountain", image:"https://farm9.staticflickr.com/8161/7360193870_cc7945dfea.jpg"},
-    {name:"Salmon Creek", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-    {name:"Sparrow Hills", image:"https://farm8.staticflickr.com/7268/7121859753_e7f787dc42.jpg"},
-    {name:"Rock Mountain", image:"https://farm9.staticflickr.com/8161/7360193870_cc7945dfea.jpg"},
-    {name:"Salmon Creek", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-    {name:"Sparrow Hills", image:"https://farm8.staticflickr.com/7268/7121859753_e7f787dc42.jpg"},
-    {name:"Rock Mountain", image:"https://farm9.staticflickr.com/8161/7360193870_cc7945dfea.jpg"}
-    
-   ];
-
+mongoose.connect("mongodb://localhost/gelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+// var campgrounds =[
+//     {name:"Salmon Creek", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+//     {name:"Sparrow Hills", image:"https://farm8.staticflickr.com/7268/7121859753_e7f787dc42.jpg"},
+//     {name:"Rock Mountain", image:"https://farm9.staticflickr.com/8161/7360193870_cc7945dfea.jpg"},
+//     {name:"Salmon Creek", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+//     {name:"Sparrow Hills", image:"https://farm8.staticflickr.com/7268/7121859753_e7f787dc42.jpg"},
+//     {name:"Rock Mountain", image:"https://farm9.staticflickr.com/8161/7360193870_cc7945dfea.jpg"},
+//     {name:"Salmon Creek", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+//     {name:"Sparrow Hills", image:"https://farm8.staticflickr.com/7268/7121859753_e7f787dc42.jpg"},
+//     {name:"Rock Mountain", image:"https://farm9.staticflickr.com/8161/7360193870_cc7945dfea.jpg"},
+//     {name:"Salmon Creek", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+//     {name:"Sparrow Hills", image:"https://farm8.staticflickr.com/7268/7121859753_e7f787dc42.jpg"},
+//     {name:"Rock Mountain", image:"https://farm9.staticflickr.com/8161/7360193870_cc7945dfea.jpg"}
+    
+//   ];
+
+var campgroundSchema = new mongoose.Schema({
+   name:String,
+   image:String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+
 
 app.get("/", function(req,res){
     res.render("landing");     
 });
 
 app.get("/campgrounds", function(req,res){
+    Campground.find({}, function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds", {campgrounds: result});
+        }
+     }).select('-__v');
    
-   res.render("campgrounds", {campgrounds: campgrounds});
+   
 });
 
 app.post("/campgrounds", function(req,res){
